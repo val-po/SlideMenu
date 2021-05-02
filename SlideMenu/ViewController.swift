0b37c4f9a95d1316dee46a782f4fc9152af270a2
VP <VP>
2021-05-02 23:03:38 +0300

    commit #1

--- SlideMenu/ViewController.swift
+++ SlideMenu/ViewController.swift
@@ -5,15 +5,59 @@
 //  Created by valentine on 02.05.2021.
 //
 
+import SideMenu
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var menu: SideMenuNavigationController?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        menu = SideMenuNavigationController(rootViewController: MenuListController())
+        menu?.leftSide = true
+        menu?.setNavigationBarHidden(true, animated: false)
+        
+        SideMenuManager.default.leftMenuNavigationController = menu
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
     }
 
-
+    @IBAction func didTapMenu() {
+        present(menu!, animated: true)
+    }
 }
 
+class MenuListController: UITableViewController {
+    
+    var items = ["First", "Second", "Third", "First", "Second", "Third", "First", "Second", "Third"]
+    
+    let darkColor = UIColor (red: 33/255.0,
+                             green: 33/255.0,
+                             blue: 33/255.0,
+                             alpha: 1)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = darkColor
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items [indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = darkColor
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // do something
+    }
+}
